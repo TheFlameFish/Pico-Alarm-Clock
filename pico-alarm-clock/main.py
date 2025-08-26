ab2abc6b01ff632d1a967e5d9adb8624e1ec87b1
TheFlameFish <114826427+TheFlameFish@users.noreply.github.com>
2025-08-26 10:16:32 -0700

    Start alarm functionality, add form to html

--- pico-alarm-clock/main.py
+++ pico-alarm-clock/main.py
@@ -23,7 +23,14 @@ lcd_i2c = SoftI2C(sda=Pin(0), scl=Pin(1), freq=400000) # For some reason it dies
 lcd = I2cLcd(lcd_i2c, LCD_ADDR, LCD_ROWS, LCD_COLS)
 
 # Buzzer setup
+ALARM_BUZZ_FREQ = 900
+BUZZER_DUTY_CYCLE = int(65536*0.2)
+
 buzzer = PWM(Pin(16))
+
+# Button setup
+button = Pin(22, Pin.IN, Pin.PULL_UP)
+alarm_screaming = False
 
 def connect_wifi():
     hostname = "placeholderalarm" # TODO This'll be smth you can adjust in config
@@ -66,6 +73,7 @@ def sync_time_api(rtc: RTC):
         print("Error: Response does not contain required data:", data)
 
 async def update_display(rtc : RTC):
+    lcd.backlight_on()
     previous_time_str = ""
     pervious_date_str = ""
 
@@ -90,10 +98,54 @@ async def update_display(rtc : RTC):
         await asyncio.sleep(1) # It'd be rather silly to refresh this more than once per second (Read: Future me, don't do that.)
 
 async def alarm():
+    global alarm_screaming
     while True:
-        # TODO Placeholder
-        print("Hai :3")
-        await asyncio.sleep(60 - rtc.datetime()[6])
+        print("Alarm tick")
+        alarm_hr, alarm_min = (None, None)
+
+        if config.data["alarm"] != None:
+            alarm_hr, alarm_min = config.data["alarm"][0], config.data["alarm"][1]
+        
+        time = rtc.datetime()
+        if time[4] == alarm_hr and time[5] == alarm_min and time[6] <= 3:
+            print("BEEEP")
+            alarm_screaming = True
+            asyncio.create_task(sound_alarm())
+
+            while button.value() == 1: # While button is unpressed
+                await asyncio.sleep(0)
+
+            alarm_screaming = False
+            print("Ok i eep now")
+
+        await asyncio.sleep(60 - rtc.datetime()[6] + 1) # Added second to make sure time is past the minute mark
+
+async def sound_alarm():
+    print("Alarm go brrrrrrr")
+    pattern = (
+        ([(0.5,0.5)] * 8) # 8 long beeps
+        + (([(0.1,0.1)] * 3 + [(0.1, 0.5)]) * 4) # 32 short beeps in groups of 4
+    )
+    
+    i = 0
+    while alarm_screaming:
+        if len(pattern) > 1:
+            times = pattern[i % (len(pattern)-1)]
+        else:
+            times = pattern[0]
+        buzzer.duty_u16(BUZZER_DUTY_CYCLE)
+        buzzer.freq(ALARM_BUZZ_FREQ)
+        await asyncio.sleep(times[0])
+
+        buzzer.duty_u16(0)
+        await asyncio.sleep(times[1])
+        i += 1
+
+async def startup_beep():
+    buzzer.duty_u16(BUZZER_DUTY_CYCLE)
+    buzzer.freq(ALARM_BUZZ_FREQ)
+    await asyncio.sleep(0.1)
+    buzzer.duty_u16(0)
 
 async def main():
     connect_wifi()
@@ -103,6 +155,7 @@ async def main():
     asyncio.create_task(update_display(rtc))
     asyncio.create_task(alarm())
     asyncio.create_task(webserver.run_server())
+    asyncio.create_task(startup_beep())
 
 loop = asyncio.get_event_loop()
 loop.create_task(main())
